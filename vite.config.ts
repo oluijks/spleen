4c50afadb0de2012336f0413a0d0e20f7331297d
Olaf Luijks <olafluijks@proton.me>
2024-06-13 22:02:41 +0200

    chore: :wrench: added svelte enhanced img plugin (for future use)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss()],
+  plugins: [
+    // @see https://kit.svelte.dev/docs/images#sveltejs-enhanced-img
+    enhancedImages(),
+    sveltekit(),
+    purgeCss(),
+  ],
 });
