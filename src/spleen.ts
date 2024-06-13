43b3167d0e75a894c71f7ed60d96a879e1d4d805
Olaf Luijks <olafluijks@proton.me>
2024-06-13 09:34:42 +0200

    refactor: :lipstick: added inter and urbanist fonts

--- src/spleen.ts
+++ src/spleen.ts
@@ -4,8 +4,8 @@ export const spleen: CustomThemeConfig = {
   name: 'spleen',
   properties: {
     // =~= Theme Properties =~=
-    '--theme-font-family-base': 'system-ui',
-    '--theme-font-family-heading': 'system-ui',
+    '--theme-font-family-base': 'Inter Variable',
+    '--theme-font-family-heading': 'Urbanist Variable',
     '--theme-font-color-base': '0 0 0',
     '--theme-font-color-dark': '255 255 255',
     '--theme-rounded-base': '9999px',
