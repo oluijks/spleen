0af8a1fcb85d5d9b2a7a78955a50617e3ac9e85d
Olaf Luijks <olafluijks@proton.me>
2024-06-13 09:02:51 +0200

    chore: :wrench: added some packages, run eslint / format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,43 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin';
-import { spleen } from './src/spleen'
+import { spleen } from './src/spleen';
 
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'wintry',
-						enhancements: true,
-					},
-					{
-						name: 'hamlindigo',
-						enhancements: true,
-					},
-					{
-						name: 'vintage',
-						enhancements: true,
-					},
-				],
-				custom: [
-					spleen,
-				],
-			},
-		}),
-	],
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    join(
+      require.resolve('@skeletonlabs/skeleton'),
+      '../**/*.{html,js,svelte,ts}'
+    ),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    forms,
+    typography,
+    skeleton({
+      themes: {
+        preset: [
+          {
+            name: 'wintry',
+            enhancements: true,
+          },
+          {
+            name: 'hamlindigo',
+            enhancements: true,
+          },
+          {
+            name: 'vintage',
+            enhancements: true,
+          },
+        ],
+        custom: [spleen],
+      },
+    }),
+  ],
 } satisfies Config;
