4069d36ab9a54051cae8699c58e9dbe4701a54ce
Olaf Luijks <olafluijks@proton.me>
2024-06-14 08:55:16 +0200

    feat: :sparkles: added sitemap (wip)

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,26 @@
+export async function GET() {
+  return new Response(
+    `		
+    <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset
+        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+      >
+        <url>
+          <loc>http://localhost:5173</loc>
+          <lastmod>2024-06-13</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.7</priority>
+        </url>
+      </urlset>`.trim(),
+    {
+      headers: {
+        'Content-Type': 'application/xml',
+      },
+    }
+  );
+}
