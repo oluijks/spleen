6ce277fe59d2378eabd813d6e3e6911f9bca6488
Olaf Luijks <olafluijks@proton.me>
2024-06-15 11:40:19 +0200

    feat: :sparkles: added about route (wip)

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { appName } from '$lib/config';
+  import { pageTitle } from '$lib/stores/app';
+
+  $pageTitle = 'about';
+</script>
+
+<div class="container mx-auto flex h-full items-center justify-center">
+  <div class="flex flex-col items-center space-y-10 text-center">
+    <h1 class="h1">
+      <span class="gradient-heading">about {appName}</span>
+    </h1>
+  </div>
+</div>
