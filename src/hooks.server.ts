7331ef6965ccbc6f195742290e467275b9f46067
Olaf Luijks <olafluijks@proton.me>
2024-06-15 13:10:56 +0200

    refactor: :stethoscope: added pocketbase health status check in server hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,42 @@
-import { type Handle } from '@sveltejs/kit';
-import { cyan, gray, yellow, bold } from 'kleur/colors';
-
-import PocketBase from 'pocketbase';
+import { PRIVATE_POCKET_BASE_URL } from '$env/static/private';
+import { appName } from '$lib/config';
+import { error, type Handle } from '@sveltejs/kit';
+import { bold as b, cyan as c, gray as g, yellow as y } from 'kleur/colors';
+import PocketBase, { ClientResponseError } from 'pocketbase';
 
 export const handle: Handle = (async ({ event, resolve }) => {
   // * Note: https://kit.svelte.dev/docs/errors#responses
   console.log(
-    gray(
-      `${cyan(bold('[spleen]'))} ${yellow(`${handle.name}()`)} src/hooks.server.ts`
-    )
+    g(`${c(b(`[${appName}]`))} ${y(`${handle.name}()`)} src/hooks.server.ts`)
   );
 
-  event.locals.pb = new PocketBase('http://127.0.0.1:8090');
+  event.locals.pb = new PocketBase(PRIVATE_POCKET_BASE_URL);
+
+  await checkPocketBaseHealthStatus(event.locals.pb);
 
   return await resolve(event);
 }) satisfies Handle;
+
+// ----------------------------------------------------------------------------
+// helper functions
+// ----------------------------------------------------------------------------
+
+const checkPocketBaseHealthStatus = async (pb: PocketBase) => {
+  try {
+    await pb.health.check();
+  } catch (error) {
+    if (error instanceof ClientResponseError) {
+      handleClientResponseError(error);
+    }
+    throw new Error('PocketBase client not available');
+  }
+};
+
+const handleClientResponseError = (health: ClientResponseError) => {
+  const { status, message } = health;
+  if (health.status === 0) {
+    error(500, message);
+  } else if (status >= 400 && status <= 599) {
+    error(status, message);
+  }
+};
